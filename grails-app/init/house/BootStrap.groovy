b315ec005734d2e0743825befe095b401a7c97e8
Ossama <omahmou2@uwo.c>
2017-03-21 18:08:02 -0400

    complete notifications

--- grails-app/init/house/BootStrap.groovy
+++ grails-app/init/house/BootStrap.groovy
@@ -2,7 +2,6 @@ package house
 /**
  * Created by gtohill on 16/02/17.
  */
-import static java.util.Calendar.YEAR
 class BootStrap {
     def init = { servletContext ->
         //ADD HOUSE
@@ -13,7 +12,9 @@ class BootStrap {
 
         Person testPerson1 = new Person(firstName:'Gary', lastName:'Tohill', subId:'107726694172578448865', email:'johnhend1974@gmail.com').save()
         Person testPerson2 = new Person(firstName: 'Rupi', lastName: 'Bains', subId: '11256632556454563', email:'rupi@gmail.com').save()
-        Person testPerson3 = new Person(firstName: 'Mark', lastName: 'Johnson', subId: '11002002022004', email:'mark@gmail.com').save()
+        Person testPerson3 = new Person(firstName: 'Ossama', lastName: 'Mahmoud', subId: '114604175836541464029', email:'omahmou2@gmail.com').save()
+
+
         /*new Person(firstName: 'Waylan', lastName: 'Smithers', sub_id: '115', houseId: '001').save()
         new Person(firstName: 'Bart', lastName: 'Simpson', sub_id: '116', houseId: '002').save()
         new Person(firstName: 'Homer', lastName: 'Simpson', sub_id: '117', houseId: '002').save()
@@ -25,6 +26,7 @@ class BootStrap {
         PersonHouse testPersonHouse1 = new PersonHouse(personId: testPerson1.subId, houseId:testHouse.houseId).save()
         PersonHouse testPersonHouse2 = new PersonHouse(personId:testPerson2.subId, houseId:testHouse.houseId).save()
         PersonHouse testPersonHouse3 = new PersonHouse(personId: testPerson3.subId, houseId:testHouse.houseId).save()
+
         Date today = new Date().minus(120)
 
         Transaction trans1 = new Transaction(invoiceId:1, houseId:1,creditorId:'107726694172578448865', debitorId:'11256632556454563', creditorName: 'Gary', debitorName: 'Rupi', amountPaid: 100, amountOwed: 50, description: 'Groceries', date:today).save()
@@ -36,6 +38,12 @@ class BootStrap {
         Score scorePerson1 = new Score(subId: testPerson1.subId, firstName: testPerson1.firstName, lastName: testPerson1.lastName, houseId:1).save()
         Score scorePerson2 = new Score(subId: testPerson2.subId, firstName: testPerson2.firstName, lastName: testPerson2.lastName, houseId:1).save()
         Score scorePerson3 = new Score(subId: testPerson3.subId, firstName: testPerson3.firstName, lastName: testPerson3.lastName, houseId:1).save()
+
+        Post post = new Post(senderName: testPerson2.firstName, title: "Title of post",
+                text: "aifbviqhbvkjsvnhnhv bhvbjjjahebv", date: (new Date()).getTimeString())
+        post.save()
+        testPerson1.addToPosts(post).save()
+        testPerson3.addToPosts(post).save()
     }
 
     def destroy = {
